17b28742622733ea5df4a974b080fe8ed2225044
C. Bates <ccbates@gmail.com>
2014-07-13 09:02:14 -0400

    init commit

---
+++ EMOTM/Model/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EMOTM.Model
+{
+    public interface IDataService
+    {
+        void GetData(Action<DataItem, Exception> callback);
+    }
+}
---
+++ EMOTM/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using GalaSoft.MvvmLight;
+using EMOTM.Model;
+
+namespace EMOTM.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IDataService _dataService;
+
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel(IDataService dataService)
+        {
+            _dataService = dataService;
+            _dataService.GetData(
+                (item, error) =>
+                {
+                    if (error != null)
+                    {
+                        // Report error here
+                        return;
+                    }
+
+                });
+        }
+
+	
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
