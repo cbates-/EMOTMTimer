a85028471bd86d8e6e5321eee7b8e7093b93baff
C. Bates <ccbates@gmail.com>
2015-12-29 18:04:32 -0500

    Appears MultiBinding is working as desired

---
+++ EMOTM/ThisMinuteTxtControl.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EMOTM
+{
+	/// <summary>
+	/// Interaction logic for ThisMinuteTxtControl.xaml
+	/// </summary>
+	public partial class ThisMinuteTxtControl : UserControl
+	{
+
+		/// <summary>
+		/// The <see cref="ThisName" /> dependency property's name.
+		/// </summary>
+		public const string NamePropertyName = "ThisName";
+
+		/// <summary>
+		/// Gets or sets the value of the <see cref="ThisName" />
+		/// property. This is a dependency property.
+		/// </summary>
+		public string ThisName
+		{
+			get
+			{
+				return (string)GetValue(ThisNameProperty);
+			}
+			set
+			{
+				SetValue(ThisNameProperty, value);
+			}
+		}
+
+		/// <summary>
+		/// Identifies the <see cref="ThisName" /> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty ThisNameProperty = DependencyProperty.Register(
+			NamePropertyName,
+			typeof(string),
+			typeof(ThisMinuteTxtControl),
+			new PropertyMetadata(null));
+		
+		public ThisMinuteTxtControl()
+		{
+			this.InitializeComponent();
+
+			//LayoutRoot.DataContext = this;
+		}
+	}
+}
