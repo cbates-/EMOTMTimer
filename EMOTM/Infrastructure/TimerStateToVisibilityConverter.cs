a6f3b9ea68c3a1ab9b172d732ebc4fbfb5620835
C. Bates <ccbates@gmail.com>
2015-12-29 21:10:53 -0500

    formatting junk

--- EMOTM/Infrastructure/TimerStateToVisibilityConverter.cs
+++ EMOTM/Infrastructure/TimerStateToVisibilityConverter.cs
@@ -5,32 +5,32 @@ using System.Windows.Data;
 
 namespace EMOTM.Infrastructure
 {
-    internal class TimerStateToVisibilityConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            Visibility ret = Visibility.Collapsed;
-	        switch ((TimerState) value)
-	        {
-			        case TimerState.Stopped:
+	internal class TimerStateToVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			Visibility ret = Visibility.Collapsed;
+			switch ((TimerState)value)
+			{
+				case TimerState.Stopped:
 					ret = Visibility.Visible;
-			        break;
-					
-					case TimerState.Started:
-					case TimerState.Paused:
+					break;
+
+				case TimerState.Started:
+				case TimerState.Paused:
 					ret = Visibility.Hidden;
-			        break;
+					break;
 
 				default:
 					ret = Visibility.Collapsed;
-			        break;
-	        }
-            return ret;
-        }
+					break;
+			}
+			return ret;
+		}
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return null;
-        }
-    }
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
 }
