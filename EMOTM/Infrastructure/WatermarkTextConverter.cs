8c1e95f6434df293ae5983492174d04bbe464347
C. Bates <ccbates@gmail.com>
2015-12-30 09:38:12 -0500

    Tune up Watermark.  Tune button size.

---
+++ EMOTM/Infrastructure/WatermarkTextConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EMOTM.Infrastructure
+{
+	public class WatermarkTextConverter : IValueConverter
+	{
+		#region IValueConverter Members
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			string watermark = "List work to do this minute";
+			string thisName = (string) value;
+
+			int indx = thisName.IndexOf("Minute", StringComparison.Ordinal);
+			watermark = string.Format("List the work{1}to do {0} minute", thisName.Substring(0, indx), Environment.NewLine);
+
+
+			return watermark;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
